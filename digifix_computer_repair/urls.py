6316072b2597c4db05d3cb4515eaee3a81c271eb
APFirebolt <aspper20@gmail.com>
2020-07-26 18:06:38 -0700

    UI changes

--- digifix_computer_repair/urls.py
+++ digifix_computer_repair/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from digifix_computer_repair import settings
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home_page.html'), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
     path('complaints/', include(('complaints.urls', 'complaints'), namespace='complaints')),
