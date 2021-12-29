3ffb853e27fc1d000cef91e4d8cbfbe8e80e0c1a
Amit <aspper20@gmail.com>
2021-12-29 21:11:20 +0530

    static root directory added

--- digifix_computer_repair/settings.py
+++ digifix_computer_repair/settings.py
@@ -148,6 +148,7 @@ LOGIN_REDIRECT_URL = '/accounts/dashboard'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
