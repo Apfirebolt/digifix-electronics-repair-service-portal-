94ff2fd2c9b0e909926572aef661818b94b39f51
Amit <aspper20@gmail.com>
2021-12-29 20:55:27 +0530

    added Procfile for heroku deployment

--- digifix_computer_repair/settings.py
+++ digifix_computer_repair/settings.py
@@ -82,14 +82,21 @@ WSGI_APPLICATION = 'digifix_computer_repair.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'digifix',
+#         'USER': 'postgres',
+#         'PASSWORD': 'pass12345',
+#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'digifix',
-        'USER': 'postgres',
-        'PASSWORD': 'pass12345',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
